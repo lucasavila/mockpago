5bc08cb29c70d2444e3521ff3875e6e8b4d796e7
Lucas Avila <lucas.avila.2010@gmail.com>
2017-10-04 22:23:20 -0300

    se agrega parseo de json

--- backend/src/main/java/com/redbee/mockpago/mockpago/controllers/MockController.java
+++ backend/src/main/java/com/redbee/mockpago/mockpago/controllers/MockController.java
@@ -1,9 +1,10 @@
 package com.redbee.mockpago.mockpago.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.*;
-
+import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -17,6 +18,8 @@ import java.util.UUID;
 @RequestMapping("/api")
 public class MockController {
 
+    private static final Logger log = Logger.getLogger(MockController.class);
+
     private String generarUUID() {
 
         String uuid = UUID.randomUUID().toString();
@@ -25,9 +28,11 @@ public class MockController {
     }
 
     @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/url", method = RequestMethod.GET, produces = "application/json")
-    public Map<String, String> getIdAndUrl() {
-
+    @RequestMapping(value = "/url", method = RequestMethod.POST, produces = "application/json")
+    public Map<String, String> getIdAndUrl(@RequestBody String jsonEntrada) {
+        log.info("jsonEntrada: " + jsonEntrada);
+        JSONObject retornoJson = new JSONObject(jsonEntrada);
+        log.info("jsonEntrada: " + retornoJson);
         Map mapa = new HashMap<String, String>();
         String uuid = generarUUID();
         String url = "sepdesa.colonseguros.com.ar/mockfront/";
